695e46b64003f8e7ec258def5b01f58c22b8c488
davidciliberto1 <davidciliberto@gmail.com>
2021-09-06 19:37:48 -0300

    readme

--- README.md
+++ README.md
@@ -1,6 +1,9 @@
 ### Temperature Controller with Bluetooth
-Description: se pueded hace blablabla
+Description: Controlador de temperatura y deshielo para freezer y heladera comercial.
+Regula la temperatura encendiendo y apagando el compresor del equipo dentro de los parametros programables, ademas, enciende y apaga una resistencia de deshielo programable a intervalos de tiempo o temperatura.
 
+Description: Temperature and defrost controller for commercial freezers.
+It regulates temperature by turning on and off the compressor in the equipment inside the parameters programmed. also has a defrost resistance programmable by time or temperature.
 ### "inserte diseño de conexion de componentes y modulos"
 
 Lista de libreria
@@ -8,3 +11,4 @@ Lista de libreria
 
 #include <LiquidCrystal.h>
 
+![alt text](image.jpg)
--- TempControllerBluetooth.ino
+++ TempControllerBluetooth.ino
@@ -5,16 +5,19 @@
 #include <math.h>
 LiquidCrystal lcd(7,6,5,4,3,2); 
 int rele = 8;
+int releDefrost = 9;
  SoftwareSerial miBT(10, 11);
  char NOMBRE[21] = "DEIVID";
  char BPS = '4';
- char PASS[5] = "0000";
+ char PASS[5] = "0000";                 // set contraseña
 void setup() 
 {     
  Serial.begin(9600);     
- Serial.println("listo"); 
+ Serial.println("listo");                               // inicializa
  miBT.begin(9600);
    pinMode(8, OUTPUT);
+  digitalWrite(rele, LOW);
+   pinMode(9, OUTPUT);
   digitalWrite(rele, LOW);
   lcd.begin(16, 2); 
   miBT.print("AT");
@@ -39,11 +42,11 @@ void loop()
     lcd.setCursor(0,0);
   int val;
   double temp;
-  val = analogRead(0);
+  val = analogRead(0);                              // asigna valores de lectura del termometro digital a una variable "temp"
   temp = log(((10240000/val) - 10000));
   temp = 1 / (0.001129148 + (0.000234125 + (0.0000000876741 * temp * temp ))* temp );
   temp = temp - 273.15;
-  Serial.print(temp);
+  Serial.print(temp);                               // imprime temperatura
   Serial.println("°C");
   lcd.print(temp);
   lcd.setCursor(6, 0);
@@ -53,11 +56,11 @@ void loop()
   delay(1000);
   
 if(temp < 27) {
-  digitalWrite(rele, LOW);
+  digitalWrite(rele, LOW);                        // apaga relay cuando temperatura sea menor que 27° 
   delay(1000);
 }
 else {
-  digitalWrite(rele, HIGH);
+  digitalWrite(rele, HIGH);                         // enciende relay haste que la temperatura sea menor a 27°
   delay(1000);
 }
   
